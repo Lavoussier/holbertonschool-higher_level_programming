4bcaeb4aa806bf13880eb70358f5178980d25389
LavoussierDev <Onilr31@gmail.com>
2024-01-31 17:13:45 -0400

    fix: wrote the wrong source code to file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+def roman_to_int(roman_string):
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    for i in range(len(roman_string)):
+        if i + 1 == len(roman_string) or romans[roman_string[i]] >= romans[roman_string[i + 1]]:
+            total += romans[roman_string[i]]
+        else:
+            total -= romans[roman_string[i]]
+
+    return total
