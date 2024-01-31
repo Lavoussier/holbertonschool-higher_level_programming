936ce5c873144dc2c97cea0313ea819431ba63bd
LavoussierDev <Onilr31@gmail.com>
2024-01-31 18:30:18 -0400

    fix: pycodestyle #2

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     for i in range(len(roman_string)):
-        if i + 1 == len(roman_string) or romans[roman_string[i]] >= romans[roman_string[i + 1]]:
+        if i + 1 == len(roman_string) or \
+                romans[roman_string[i]] >= romans[roman_string[i + 1]]:
             total += romans[roman_string[i]]
         else:
             total -= romans[roman_string[i]]
