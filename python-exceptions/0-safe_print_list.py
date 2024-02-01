913fec9b2df3a9a317ed8c14203256fbfa2b67ce
LavoussierDev <Onilr31@gmail.com>
2024-02-01 06:55:40 -0400

    fix: returning a value

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
@@ -6,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
         print("")
     except IndexError:
         print("")
-        return i + 1
+        return i + 1 if i < len(my_list) else i
     return x
