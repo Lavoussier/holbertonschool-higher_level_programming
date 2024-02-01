bb36c4c34b6580764f3ea7bdec60215aaf9535d2
LavoussierDev <Onilr31@gmail.com>
2024-02-01 06:59:13 -0400

    fix: not using len

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
+        for y in range(0, x):
+            print("{}".format(my_list[y]), end="")
         print("")
     except IndexError:
         print("")
-        return i if i < len(my_list) else i + 1
+        return y
     return x
