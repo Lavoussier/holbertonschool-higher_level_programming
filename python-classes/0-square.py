99baa2c9cf41f237c74c9d785f44de074eb19082
LavoussierDev <Onilr31@gmail.com>
2024-02-02 01:27:11 -0400

    docs: small changes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """
-    This is an empty class representing a square.
-
-    Attributes:
-        No attributes defined for an empty Square class.
-"""
+    This is an empty class representing a square."""
 class Square:
-        Methods:
     """ No methods defined for an empty Square class. """
     pass
