e80599f28099d8925f103a8c624ac8038d01e9f2
LavoussierDev <Onilr31@gmail.com>
2024-02-02 23:26:04 -0400

    Upload: source code

---
+++ python-classes/6-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Instantiation with optional size and position"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Property to retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("Size must be an integer")
+        elif value < 0:
+            raise ValueError("Size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """Property to retrieve position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Property setter for position"""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("Position must be a tuple of 2 positive integers")
+        elif not all(isinstance(coord, int) and coord >= 0 for coord in value):
+            raise TypeError("Position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """Public instance method that returns the square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Public instance method that prints the square with the char #"""
+        if self.__size == 0:
+            print("")
+            return
+
+        rows, cols = self.__position[1], self.__position[0]
+        print("\n" * rows, end="")
+        for _ in range(self.__size):
+            print(" " * cols + "#" * self.__size)
