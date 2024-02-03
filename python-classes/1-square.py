daf3ad87abe3f7ac606b3fcdcf99d47d3bfd59a8
LavoussierDev <Onilr31@gmail.com>
2024-02-02 23:05:10 -0400

    Upload: file

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
+
+
+class Square:
+    """Square class with a size attribute"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
