8c8d052ce34fb60cbfd794662ddb146570cc32d3
LavoussierDev <Onilr31@gmail.com>
2024-02-02 23:38:22 -0400

    docs: comment

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -43,7 +43,8 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Public instance method that prints the square with the character #"""
+        """Public instance method that
+        prints the square with the character #"""
         if self.__size == 0:
             print("")
             return
